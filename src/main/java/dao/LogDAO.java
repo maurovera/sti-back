fbb16f7d4c4740623ad199735f1100dd06ad10ba
mauro <mau4250@gmail.com>
2018-09-18 20:13:59 -0400

    Crea un archivo de log
    Creacion del archivo que guarda el paso
    por el segundo test.

--- src/main/java/dao/LogDAO.java
+++ src/main/java/dao/LogDAO.java
@@ -1,7 +1,13 @@
 package dao;
 
-import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+import utils.AppException;
+import model.Evidencia;
 import model.Log;
 import base.BaseDAO;
 
@@ -13,4 +19,25 @@ public class LogDAO extends BaseDAO<Log>{
 	
 		return Log.class;
 	}
+	
+	
+	
+	/**Lista de log
+	 **/
+	public List<Log> listaLog() throws AppException {
+
+		System.out.println("Lista de log");
+		List<Log> lista = new ArrayList<Log>();
+		
+		Query q = em.createQuery("SELECT l FROM Log l ");
+		lista = q.getResultList();
+
+
+		if (lista == null) {
+			throw new AppException(404, "Not Found lista log");
+		}
+
+		return lista;
+	}
+
 }
--- src/main/java/resource/LogResource.java
+++ src/main/java/resource/LogResource.java
@@ -2,9 +2,14 @@ package resource;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import model.Log;
 import service.LogService;
@@ -24,6 +29,31 @@ public class LogResource  extends BaseResource<Log, LogService>  {
 	public LogService getService() {
 		// TODO Auto-generated method stub
 		return service;
+	}
+	
+	
+	/***
+	 * Crea el archivo log
+	 **/
+	@GET
+	@Path("/archivoLog")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String archivolog() {
+		System.out.println("Crear un archivo log resource");
+
+		String archivo = null;
+		try {
+			archivo = getService().escribirArchivoLog();
+		} catch (Exception e) {
+			throw new WebApplicationException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		}
+		if (archivo == null) {
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+
+		
+		return archivo;
 	}
 
 }
